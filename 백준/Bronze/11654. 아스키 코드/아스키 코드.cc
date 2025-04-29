54038bdaf7ac9479aa16c5a04c5eb1f7c1327f17
menare <132678309+chominha@users.noreply.github.com>
2025-04-29 09:59:01 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::ios::sync_with_stdio(false); 
+    std::cin.tie(NULL);
+    
+    char ch;
+    std::cin>>ch;
+    std::cout<<(int)ch;
+
+    return 0;
+}
