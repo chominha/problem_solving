930d89dac02896950e2232df78cdbe82633f867b
menare <132678309+chominha@users.noreply.github.com>
2025-05-11 16:20:55 +0900

    Create 시간초과 나는야 포켓몬 마스터 이다솜.cc
    timeout

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/시간초과 나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <cctype>
+
+int main()
+{
+    int libraryNum = 0, myNum = 0;
+    std::cin >> libraryNum >> myNum;
+
+    std::string name;
+    std::string yourInput;
+    std::map<int, std::string> pokemonMap;
+    std::vector<std::string> answer;
+
+    for (int i = 1; i <= libraryNum; i++) {
+        std::cin >> name;
+        pokemonMap.insert({i, name});
+    }
+
+    for (int i = 0; i < myNum; i++) {
+        std::cin >> yourInput;
+
+        if (std::isdigit(yourInput[0])) {
+            int index = std::stoi(yourInput);
+            answer.push_back(pokemonMap[index]);
+        }
+        else {
+            for (const auto& pair : pokemonMap) {
+                if (pair.second == yourInput) {
+                    answer.push_back(std::to_string(pair.first));
+                    break;
+                }
+            }
+        }
+    }
+
+    for (const auto& a : answer) {
+        std::cout << a << "\n";
+    }
+
+    return 0;
+}
+
+/*
+isdigit(ch) : 문자가 숫자인지 확인(문자 하나만 검사!)
+stoi(str) : 문자열을 정수로 변환
+to_string : 정수를 문자열로 변환
+
+자료형에 상관없이 저장!
+std::vector<std::any> : 안전하진 않아
+using VarType = std::variant<int, double, std::string>;
+std::vector<VarType> list; : 특정 자료형만 안전.
+chat gpt방법 : map 두개써서 <int,string>이랑 <string,int> 두개 만들어라.(시간복잡도 차이)
+*/
