8da152a85eb27d7c6b27037d30a0f62e6ee4d4ca
menare <132678309+chominha@users.noreply.github.com>
2025-05-13 10:42:42 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    std::vector<std::string> answer;
+    std::string str;
+    int num=0;
+    std::cin>>num;
+    for(int i=0; i<num; i++){
+        bool isTrue = true;
+        std::stack<char> check;
+    
+        std::cin>>str;
+        for(char a : str){
+            if(a=='('){
+                check.push('c');
+            }
+            else if(a==')'){
+                if(!check.empty()){
+                   check.pop(); 
+                }
+                else{
+                    isTrue = false;
+                    break;
+                }
+            }
+        }
+        if(isTrue && check.empty()){
+            answer.push_back("YES");
+        }else{
+            answer.push_back("NO");
+        }
+    }
+    for(std::string a : answer){
+        std::cout <<a<<"\n";
+    }
+
+    return 0;
+}
+/*
+bool 초기화 위치 주의.
+*/
