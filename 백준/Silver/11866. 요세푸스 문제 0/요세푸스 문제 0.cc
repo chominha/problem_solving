be4d39e78090c9fbdac06828b2d1cffaa442725b
menare <132678309+chominha@users.noreply.github.com>
2025-05-15 23:30:52 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+int main()
+{
+    int num=0,c=0;
+    std::cin>>num>>c;
+    std::queue<int> yo;
+    std::vector<int> answer;
+    
+    for(int i=1; i<num+1; i++){
+        yo.push(i);
+    }
+    while(!yo.empty()){
+        for(int i=0; i<c-1; i++){
+            yo.push(yo.front());
+            yo.pop();
+        }
+        answer.push_back(yo.front());
+        yo.pop();
+    }
+    std::cout<<"<";
+    for(int i : answer){
+        std::cout<<i;
+        if (i != answer.back()){
+            std::cout << ", ";
+        } 
+    }
+    std::cout<<">";
+
+    return 0;
+}
+
+/*
+요세푸스
+큐를 활용하여. 
+*/
