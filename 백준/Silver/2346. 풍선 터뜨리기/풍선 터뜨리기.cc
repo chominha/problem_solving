7c9c8ee6f9430b52bcf1b3a948221af8a7e82e9c
menare <132678309+chominha@users.noreply.github.com>
2025-05-18 17:38:43 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <deque>
+#include <vector>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int ballcount = 0;
+    std::cin >> ballcount;
+
+    std::deque<std::pair<int, int>> balldeque;
+    std::vector<int> answer;
+
+    for (int i = 0; i < ballcount; i++) {
+        int num;
+        std::cin >> num;
+        balldeque.push_back(std::make_pair(i, num));
+    }
+
+    while (!balldeque.empty()) {
+        int index = balldeque.front().first;
+        int move = balldeque.front().second;
+        balldeque.pop_front();
+
+        answer.push_back(index+1);
+
+        if (balldeque.empty()) break;
+
+        if (move > 0) {
+            for (int i = 0; i < move - 1; i++) {
+                std::pair<int, int> temp = balldeque.front();
+                balldeque.pop_front();
+                balldeque.push_back(temp);
+            }
+        } else {
+            for (int i = 0; i < -move; i++) {
+                std::pair<int, int> temp = balldeque.back();
+                balldeque.pop_back();
+                balldeque.push_front(temp);
+            }
+        }
+    }
+
+    for (int i = 0; i < answer.size(); i++) {
+        std::cout << answer[i] << " ";
+    }
+
+    return 0;
+}
+
+/*
+맨처음 1번 풍선 -> 1 + 1번 풍선 값 => 4 , 1번 pop -> 재정렬
+4번 풍선 -> 4 + 4번 풍선 값 => 5
+*/
