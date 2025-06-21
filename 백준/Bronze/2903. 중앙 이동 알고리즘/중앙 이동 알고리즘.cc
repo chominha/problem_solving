a9e3160317d8699f999569200b0e19a65a27abea
menare <132678309+chominha@users.noreply.github.com>
2025-06-21 23:28:17 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
@@ -0,0 +1,12 @@
+//2903
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, answer;
+    cin >> n;
+    int num = pow(2,n) +1;
+    answer = num*num;
+    cout<<answer;
+    return 0;
+}
