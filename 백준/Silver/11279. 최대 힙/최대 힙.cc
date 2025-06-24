a3a9cf6c0af252b48b5da1bb302094c1cb0a814c
menare <132678309+chominha@users.noreply.github.com>
2025-06-24 12:42:15 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int num;
+    std::cin>>num;
+    std::priority_queue<int> answer;
+    for(int i=0; i<num; i++){
+        int one;
+        std::cin>>one;
+        if(one==0){
+            if(answer.empty()){
+                std::cout<<0<<"\n";
+            }else{
+            std::cout<<answer.top()<<"\n";
+            answer.pop();
+            }
+        }else{
+            answer.push(one);
+        }
+    }
+
+    return 0;
+}
