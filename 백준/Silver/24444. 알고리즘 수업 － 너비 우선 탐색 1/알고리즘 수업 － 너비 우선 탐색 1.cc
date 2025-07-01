33886c9d019318406496f27e2c479b7714249652
menare <132678309+chominha@users.noreply.github.com>
2025-07-01 20:12:57 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 236 ms, Memory: 9452 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+std::vector<std::vector<int>> graph;
+std::vector<bool> visited;
+std::vector<int> order;
+int cnt = 0;
+
+void bfs(int node){
+    std::queue<int> q;
+    visited[node] = true;
+    q.push(node);
+    order[node] = ++cnt;
+
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+
+        for (int next : graph[cur]) {
+            if (!visited[next]) {
+                visited[next] = true;
+                q.push(next);
+                order[next] = ++cnt;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int N,M,R;
+    std::cin>>N>>M>>R;
+    graph.resize(N+1);
+    visited.resize(N+1, false);
+    order.resize(N + 1, 0);
+    
+    for(int i=0; i<M; i++){
+        int u,v;
+        std::cin>>u>>v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    for (int i = 1; i <= N; i++) {
+        sort(graph[i].begin(), graph[i].end());
+    }
+    
+    bfs(R);
+    
+    for (int i = 1; i <= N; i++) {
+        std::cout << order[i] << "\n";
+    }
+
+    return 0;
+}
